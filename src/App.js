285a696a2e0cca0f7ff58e6fbcb448d306b5c8b4
michaelremes <michael.remes@gmail.com>
2019-12-26 17:28:00 +0100

    uvodniStranka

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './EduLogo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vítejte do výukového systemu SelfEd made by michael Remes
-        </a>
+          Vítejte do výukového systemu SelfEd
       </header>
     </div>
   );
