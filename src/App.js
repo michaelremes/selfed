8a7ad22a374d5adb40c3ec72ac8cc0975a74acc7
michaelremes <michael.remes@gmail.com>
2019-12-26 16:51:15 +0100

    test comm

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Vítejte do výukového systemu SelfEd
+          Vítejte do výukového systemu SelfEd made by michael Remes
         </a>
       </header>
     </div>
