3d7f48f967bb3e067c310f3d7caa455c5fee9c35
michaelremes <michael.remes@gmail.com>
2020-02-23 22:08:00 +0100

    login and dashboard prototype

--- client/app/components/Dashboard/Dashboard.js
+++ client/app/components/Dashboard/Dashboard.js
@@ -8,11 +8,40 @@ import logo from "../../../public/assets/img/EduLogo.png";
 class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoading: true,
+      token: '',
+    };
 
     this.logout = this.logout.bind(this);
     };
 
 
+  componentDidMount() {
+    const obj = getFromStorage('the_main_app');
+    if (obj && obj.token) {
+      const { token } = obj;
+      // Verify token
+      fetch('/api/account/verify?token=' + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token,
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false,
+            });
+          }
+        });
+    } else {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  }
 
 
 
