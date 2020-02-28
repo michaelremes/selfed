e287665ff5b8b7c9c6d0f8a16efc537c820b2d6b
michaelremes <michael.remes@gmail.com>
2020-02-28 23:12:43 +0100

    Front end for creating new question prototype

--- client/app/components/Questions/Questions.js
+++ client/app/components/Questions/Questions.js
@@ -3,29 +3,80 @@ import {Button, FormGroup, FormControl} from "react-bootstrap";
 import "../../styles/Questions/Questions.css";
 
 
-
 class Questions extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      questionType: 'text'
+    };
 
+    this.onSelectCorrectAnswer = this.onSelectCorrectAnswer.bind(this);
+  }
+
+  onSelectCorrectAnswer(event) {
+    this.setState({
+      questionType: event.target.value,
+    });
+  }
+  renderCorrectAnswer(param) {
+    switch(param) {
+      case 'text':
+        return <FormControl
+          type="text"
+        />;
+      case 'checkbox':
+        return <input
+          name="isGoing"
+          type="checkbox"
+        />;
+      case 'radio':
+        return <input
+          name="isGoing2"
+          type="radio"
+        />;
+      default:
+        return null;
+    }
+  }
 
   render() {
     return (
       <div>
+        <header className="Questions-header">
+          Vytvořit otázku
+        </header>
+        <div className="CreateQuestion">
+          <form>
+            <h2>Název otázky</h2>
+            <FormGroup controlId="task" bsSize="large">
+              <FormControl
+                type="text"
+              />
+            </FormGroup>
+            <h2>Zadání</h2>
+            <FormGroup controlId="task" bsSize="large">
+              <FormControl
+                type="text"
+              />
+            </FormGroup>
+            <h2>Typ odpovědi</h2>
+            <select id="selectAnswer" onChange={this.onSelectCorrectAnswer}>
+              <option value="text">Otevřená odpověd</option>
+              <option value="checkbox">Více odpovědí</option>
+              <option value="radio">Jedná správná odpověď</option>
+            </select>
 
-            <header className="Questions-header">
-              Vytvořit otázku
-            </header>
-      <div className="CreateQuestion">
-      <form>
-        <FormGroup controlId="textInput" bsSize="large">
-          <FormControl
-            type="username"
-            placeholder="Uživatelské jméno"
+            <h2>Správná odpověď</h2>
 
-          />
-        </FormGroup>
-      </form>
-      </div>
+            {/*{this.state.questionType}*/}
+        <div>
+        {this.renderCorrectAnswer(this.state.questionType)}
+        </div>
+
+
+          </form>
+        </div>
       </div>
     );
   }
---
+++ server/routes/api/createTest.js
@@ -0,0 +1,9 @@
+
+const Question = require('../../models/Question');
+
+module.exports = (app) => {
+
+
+}
+
+
