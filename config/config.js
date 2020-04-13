fbc092d72a58d47968bd25ca8cc3db1d1180d6ab
michaelremes <michael.remes@gmail.com>
2020-04-13 22:28:13 +0200

    prod testing

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://username:password@url:port/db',
+  db: 'mongodb://user:HesNaMon883#@ds143242.mlab.com:43242/heroku_0zjc01nx',
   db_dev: 'mongodb://127.0.0.1:27017/selfed',
 
 };
