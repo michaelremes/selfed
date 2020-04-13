366c4f73a4f66f26750c878a564c5d0b336ecb59
michaelremes <michael.remes@gmail.com>
2020-04-14 00:32:37 +0200

    prod testing

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://user:HesNaMon883#@ds143242.mlab.com:43242/heroku_0zjc01nx',
-  db_dev: 'mongodb://127.0.0.1:27017/selfed',
+  db: 'mongodb://user:mongoDB883#@ds143242.mlab.com:43242/heroku_0zjc01nx',
+  db_dev: 'mongodb://user:mongoDB883@ds143242.mlab.com:43242/heroku_0zjc01nx',
 
 };
 
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,13 +1,24 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
 
-  output: {
-    filename: 'js/[name].[hash].js',
-    chunkFilename: '[id].[hash].chunk.js'
-  },
+    output: {
+      filename: 'js/[name].[hash].js',
+      chunkFilename: '[id].[hash].chunk.js'
+    },
+
+    plugins: [
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
 
   }
 );
