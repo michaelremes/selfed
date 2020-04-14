f1fc4b12d3128642fcf9009c6eb49398c49d3832
michaelremes <michael.remes@gmail.com>
2020-04-14 10:11:17 +0200

    prod init

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://admin:monherDB883#@ds143242.mlab.com:43242/heroku_0zjc01nx',
+  db: 'mongodb+srv://michaelremes:HesNaMon883@cluster0-plwhl.gcp.mongodb.net/test?retryWrites=true&w=majority',
   db_dev: 'mongodb://127.0.0.1:27017/selfed',
 
 };
